56df590f329d29ab2c01ed690bb34009692c9915
lzx5792505 <375159680@qq.com>
2022-04-27 15:59:43 +0800

    集成Echarts

---
+++ src/components/Echarts.jsx
@@ -0,0 +1,38 @@
+import { useRef as Refs, useEffect as Effect } from 'react'
+import * as echarts from 'echarts'
+
+export default function Index ({ title,  xData, yData, style}) {
+    const domRef = Refs()
+
+    const chartsInit = () => {
+        const myChart = echarts.init(domRef.current)
+
+        myChart.setOption({
+            title:{
+                text:title
+            },
+            tooltop:{},
+            xAxis:{
+                data:xData
+            },
+            yAxis:{},
+            series:[
+                {
+                    name:'123',
+                    type:'bar',
+                    data:yData
+                }
+            ]
+        })
+    }
+
+    Effect(() => {
+        chartsInit()
+    })
+
+    return (
+        <>
+            <div ref={ domRef } className={ style }></div>
+        </>
+    )
+}
--- src/view/home/Home.jsx
+++ src/view/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Echarts from '@/components/Echarts'
+import '@/assets/scss/home.scss'
 
-export default function Home() {
+export default function Index () {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <Echarts title="1111" xData={['1','2','3','4']} yData={[1,2,3,4]}  style="home-item" />
+      <Echarts title="1111" xData={['1','2','3','4']} yData={[1,2,3,4]}  style="home-item-l" />
+    </div>
   )
 }
